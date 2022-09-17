b0f709ba006101e09bc25dcda410edc1b6498c8d
Anna Gubskaya <gubskaya.ann@gmail.com>
2022-09-17 13:18:49 +0500

    update path to images

--- src/js/components/button.js
+++ src/js/components/button.js
@@ -4,7 +4,7 @@
 
   function alertS() {
     btnClick.innerHTML = 'loading...';
-    document.body.style.backgroundImage = 'url(../img/background_02.png)';
+    document.body.style.backgroundImage = 'url(./img/background_02.png)';
     document.body.style.transition = 'all .4s';
     setTimeout(function() {
       document.body.style.background = "";
